db0cc5344d25c1798e568484b82bd864926cff58
NhatPham <43815615+JackcFam@users.noreply.github.com>
2021-06-09 17:06:37 +0700

    Update test.php

--- test.php
+++ test.php
@@ -29,7 +29,7 @@ function sort_array($array=array()) {
     $length_threshold = $length_array / 3;
     $newArr = [];
     for ($i = 0; $i < $length_threshold; $i++) {
-    array_push($newArr,slice($array,(i * 3, (i + 1) * 3)));
+    array_push($newArr,array_slice($array,(i * 3, (i + 1) * 3)));
     }
     $result = [];
     for ($i = 0; $i < count($newArr); $i++) {
